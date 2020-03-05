db9678288054dba85f5ce8959c20cc2436f7a1fa
Hugo <hugovk@users.noreply.github.com>
2020-03-05 23:55:56 +0200

    Show more than bytes for negative file sizes

--- src/humanize/filesize.py
+++ src/humanize/filesize.py
@@ -10,7 +10,7 @@ suffixes = {
 
 
 def naturalsize(value, binary=False, gnu=False, format="%.1f"):
-    """Format a number of byteslike a human readable filesize (eg. 10 kB).  By
+    """Format a number of bytes like a human readable filesize (eg. 10 kB).  By
     default, decimal suffixes (kB, MB) are used.  Passing binary=true will use
     binary suffixes (KiB, MiB) are used and the base will be 2**10 instead of
     10**3.  If ``gnu`` is True, the binary argument is ignored and GNU-style
@@ -25,19 +25,20 @@ def naturalsize(value, binary=False, gnu=False, format="%.1f"):
 
     base = 1024 if (gnu or binary) else 1000
     bytes = float(value)
+    abs_bytes = abs(bytes)
 
-    if bytes == 1 and not gnu:
-        return "1 Byte"
-    elif bytes < base and not gnu:
+    if abs_bytes == 1 and not gnu:
+        return "%d Byte" % bytes
+    elif abs_bytes < base and not gnu:
         return "%d Bytes" % bytes
-    elif bytes < base and gnu:
+    elif abs_bytes < base and gnu:
         return "%dB" % bytes
 
     for i, s in enumerate(suffix):
         unit = base ** (i + 2)
-        if bytes < unit and not gnu:
+        if abs_bytes < unit and not gnu:
             return (format + " %s") % ((base * bytes / unit), s)
-        elif bytes < unit and gnu:
+        elif abs_bytes < unit and gnu:
             return (format + "%s") % ((base * bytes / unit), s)
     if gnu:
         return (format + "%s") % ((base * bytes / unit), s)
