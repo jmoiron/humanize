aba0ea6870559ae9852159c63d66d1a88c4865e0
Álvaro Mondéjar <mondejar1994@gmail.com>
2020-08-06 14:16:44 +0200

    Split new i18n tests into separate functions.

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -21,36 +21,41 @@ def test_i18n():
         assert humanize.ordinal(5) == "5th"
 
 
-def test_i18n_default_locale_path():
-    # __file__ defined
+def test_default_locale_path_defined__file__():
     i18n = importlib.import_module("humanize.i18n")
     assert i18n.get_default_locale_path() is not None
 
-    # __file__ is None
+
+def test_default_locale_path_null__file__():
+    i18n = importlib.import_module("humanize.i18n")
     i18n.__file__ = None
     assert i18n.get_default_locale_path() is None
 
-    # __file__ not defined
+
+def test_default_locale_path_undefined__file__():
+    i18n = importlib.import_module("humanize.i18n")
     del i18n.__file__
     i18n.get_default_locale_path() is None
 
 
-def test_activate_without_default_locale_path():
+class TestActivate:
     expected_msg = (
         "Humanize cannot determinate the default location of the"
         " 'locale' folder. You need to pass the path explicitly."
     )
 
-    # __file__ is None
-    i18n = importlib.import_module("humanize.i18n")
-    i18n.__file__ = None
+    def test_default_locale_path_null__file__(self):
+        i18n = importlib.import_module("humanize.i18n")
+        i18n.__file__ = None
 
-    with pytest.raises(Exception) as excinfo:
-        i18n.activate("ru_RU")
-    assert str(excinfo.value) == expected_msg
+        with pytest.raises(Exception) as excinfo:
+            i18n.activate("ru_RU")
+        assert str(excinfo.value) == self.expected_msg
 
-    # __file__ not defined
-    del i18n.__file__
-    with pytest.raises(Exception) as excinfo:
-        i18n.activate("ru_RU")
-    assert str(excinfo.value) == expected_msg
+    def test_default_locale_path_undefined__file__(self):
+        i18n = importlib.import_module("humanize.i18n")
+        del i18n.__file__
+
+        with pytest.raises(Exception) as excinfo:
+            i18n.activate("ru_RU")
+        assert str(excinfo.value) == self.expected_msg
