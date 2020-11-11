d97c61c00375f7c70654d5901cc39e2e22d63854
Thomas Grainger <tagrain@gmail.com>
2020-11-11 18:55:29 +0000

    move setuptools_scm config to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
-def local_scheme(version):
-    """Skip the local version (eg. +xyz of 0.6.1.dev4+gdf99fe2)
-    to be able to upload to Test PyPI"""
-    return ""
-
-
-setup(use_scm_version={"local_scheme": local_scheme})
+setup()
